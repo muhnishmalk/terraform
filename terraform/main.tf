fe510ba2852fff5e4acdc9cbcb26a1bf48cfef4c
muhnishmalk <nishmalk94@gmail.com>
2024-05-30 16:16:29 +0530

    main file update

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,4 +6,5 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-07543813a68cc4fe9"
   instance_type = "t2.micro"
+  key_name      = "my-key"  # Replace with your key pair name
 }
